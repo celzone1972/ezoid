288bc3722c7f19aa20792fd8ae45439fd3a7c531
celzone1972 <celzone1972@gmail.com>
2026-07-14 11:58:01 +0700

    Update visitor.js

--- api/visitor.js
+++ api/visitor.js
@@ -2,20 +2,20 @@ export default async function handler(req, res) {
   const token = process.env.TELEGRAM_BOT_TOKEN;
   const chatId = process.env.TELEGRAM_CHAT_ID;
 
-  const message = "🚀 Ada pengunjung baru membuka website EZOID";
-
   const url = `https://api.telegram.org/bot${token}/sendMessage`;
 
-  await fetch(url, {
+  const response = await fetch(url, {
     method: "POST",
     headers: {
       "Content-Type": "application/json"
     },
     body: JSON.stringify({
       chat_id: chatId,
-      text: message
+      text: "🚀 Ada pengunjung baru membuka website EZOID"
     })
   });
 
-  res.status(200).json({ status: "OK" });
+  const result = await response.json();
+
+  res.status(200).json(result);
 }
