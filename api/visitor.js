0489ff830fa53d9a679a4a82a4894719639458a8
celzone1972 <celzone1972@gmail.com>
2026-07-15 15:15:15 +0700

    Update visitor.js

--- api/visitor.js
+++ api/visitor.js
@@ -98,5 +98,7 @@ export default async function handler(req, res) {
 
   const result = await response.json();
 
-  res.status(200).json(result);
+console.log("Telegram response:", result);
+
+res.status(200).json(result);
 }
