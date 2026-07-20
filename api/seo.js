3e3ce258cd3a15832d3d455c8868cfbeced56afd
celzone1972 <celzone1972@gmail.com>
2026-07-21 00:57:03 +0700

    Update seo.js

--- api/seo.js
+++ api/seo.js
@@ -1,8 +1,71 @@
-export default function handler(req, res) {
+import { google } from "googleapis";
 
-  res.status(200).json({
-    status: "SEO API EZOID aktif",
-    message: "Siap terhubung dengan Google Search Console"
-  });
+export default async function handler(req, res) {
+
+  try {
+
+    const credentials = JSON.parse(
+      process.env.GOOGLE_SERVICE_ACCOUNT_JSON
+    );
+
+
+    const auth = new google.auth.GoogleAuth({
+
+      credentials,
+
+      scopes: [
+        "https://www.googleapis.com/auth/webmasters.readonly"
+      ]
+
+    });
+
+
+    const searchconsole = google.searchconsole({
+      version: "v1",
+      auth
+    });
+
+
+    const response = await searchconsole.searchanalytics.query({
+
+      siteUrl: "https://ezoid.vercel.app/",
+
+      requestBody: {
+
+        startDate: "2026-07-01",
+
+        endDate: "2026-07-20",
+
+        dimensions: [
+          "query"
+        ],
+
+        rowLimit: 10
+
+      }
+
+    });
+
+
+    res.status(200).json({
+
+      status: "SEO EZOID aktif",
+
+      data: response.data
+
+    });
+
+
+  } catch(error) {
+
+
+    res.status(500).json({
+
+      error: error.message
+
+    });
+
+
+  }
 
 }
