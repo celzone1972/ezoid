0f27682d322d59623c427ea2a19a170b91a5bc2d
celzone1972 <celzone1972@gmail.com>
2026-07-21 00:37:54 +0700

    Update seo.js

--- api/seo.js
+++ api/seo.js
@@ -1,37 +1,8 @@
-Skip to content
-celzone1972
-ezoid
-Repository navigation
-Code
-Issues
-Pull requests
-Actions
-Projects
-Wiki
-Security and quality
-1
- (1)
-Insights
-Settings
-ezoid/api
-/
-seo.js
-in
-main
+export default function handler(req, res) {
 
-Edit
+  res.status(200).json({
+    status: "SEO API EZOID aktif",
+    message: "Siap terhubung dengan Google Search Console"
+  });
 
-Preview
-Indent mode
-
-Spaces
-Indent size
-
-2
-Line wrap mode
-
-No wrap
-Editing seo.js file contents
-  1
-
-Use Control + Shift + m to toggle the tab key moving focus. Alternatively, use esc then tab to move to the next interactive element on the page.
+}
