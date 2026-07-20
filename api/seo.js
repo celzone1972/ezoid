6befd02dafb8df0b2ad9b439b2591d8dd877a9e0
celzone1972 <celzone1972@gmail.com>
2026-07-21 00:25:37 +0700

    Create seo.js

---
+++ api/seo.js
@@ -0,0 +1,37 @@
+Skip to content
+celzone1972
+ezoid
+Repository navigation
+Code
+Issues
+Pull requests
+Actions
+Projects
+Wiki
+Security and quality
+1
+ (1)
+Insights
+Settings
+ezoid/api
+/
+seo.js
+in
+main
+
+Edit
+
+Preview
+Indent mode
+
+Spaces
+Indent size
+
+2
+Line wrap mode
+
+No wrap
+Editing seo.js file contents
+  1
+
+Use Control + Shift + m to toggle the tab key moving focus. Alternatively, use esc then tab to move to the next interactive element on the page.
